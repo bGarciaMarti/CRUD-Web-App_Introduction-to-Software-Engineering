8c2eb6ee029c824a8253147e2ee9f1982d045ff5
Bren Garcia Marti <bdenny@uccs.edu>
2023-04-05 16:30:55 +0000

    scaffolding up and running

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   resources :player_characters
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+#  Defines the root path route ("/")
+  # root "articles#index"
+  root "player_characters#index"
+  resources :player_characters
+  
 end
