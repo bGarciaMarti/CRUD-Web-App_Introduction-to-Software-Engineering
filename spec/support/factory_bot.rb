4f87f08a18c65fc14ee93a1169d2a39195611c6e
Bren garcia marti <bdenny@uccs.edu>
2023-04-13 22:54:06 +0000

    login user spec test working SOMEHOW

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -1,3 +1,18 @@
+module ControllerMacros
+    def login_user
+      # Before each test, create and login the user
+      before(:each) do
+        @request.env["devise.mapping"] = Devise.mappings[:user]
+        user = FactoryBot.create(:user)
+        # user.confirm! # Or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
+        sign_in user
+      end
+    end
+end  
+
 RSpec.configure do |config|
+    config.include Devise::Test::ControllerHelpers, :type => :controller
+    config.include Devise::Test::IntegrationHelpers, :type => :feature
     config.include FactoryBot::Syntax::Methods
+    config.extend ControllerMacros, :type => :controller
  end 
